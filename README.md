0c147a4a1e214de08128072b3b2c0a6e1b9f1bbe
vikornienko <vikornienko76@gmail.com>
2025-03-08 15:57:30 +0500

    Python-decouple settings.

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ For installing dependencies use command: `uv add celery django-allauth django-ce
 
 For dev dependencies: `uv add --dev pytest pytest-django`.
 
+For using python-decouple:
+- create the .env file in directoty with db file;
+- in the settings.py file add 
+from decouple import config
+
+...
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', cast=bool)
+
+...
+
--- djangoRecomm/djangoRecomm/settings.py
+++ djangoRecomm/djangoRecomm/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@&tl#gj^^qfpb05-m=jfv*h*np%nc=m@jy5@0(6ax)y4$i=r6n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
